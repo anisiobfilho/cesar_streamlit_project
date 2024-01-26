576d1e3cf34e7474f017cbc12ca26a18423981e4
Anísio Filho <anisiobfilho@gmail.com>
2024-01-26 19:22:33 -0300

    fourth commit

--- pages/Análise_de_Dados.py
+++ pages/Análise_de_Dados.py
@@ -23,7 +23,7 @@ def carrega_base(path):
 
 tokyo2020 = carrega_base("data/tokyo_2020_swim.csv")
 
-st.subheader("Base de Dados Pré-processada")
+st.subheader("Base de dados pré-processada")
 
 tokyo2020.dropna(subset=['place', 'lane'], inplace=True)
 tokyo2020.loc[:, 'place'] = tokyo2020['place'].astype(int)
@@ -42,73 +42,95 @@ tokyo2020.drop(['dq'], axis=1, inplace=True)
 
 st.dataframe(tokyo2020, use_container_width=True)
 
+st.subheader("Tempo de reação")
+col1, col2 = st.columns([1,2.5])
+with col1:
+    event_selected = st.selectbox('Selecione o evento que você gostaria de analisar?', (tokyo2020.event.unique()), key=1)
+with col2:
+    tempoReacao = tokyo2020[(tokyo2020['event']== event_selected) & (tokyo2020.heat.str.startswith('heat_'))]
+    fig1 = px.scatter(tempoReacao, 
+                    x="reaction_time", 
+                    y="time", 
+                    trendline="ols", 
+                    title=f"Tempo de reação no evento: {event_selected}")
+    st.plotly_chart(fig1, use_container_width=True)
 
-st.subheader("Tempo de reação: Eliminatória Women 50 Freestyle")
+st.subheader("Enventos finais")
+col3, col4 = st.columns([1,2.5])
+with col3:
+    event_selected2 = st.selectbox('Selecione o evento que você gostaria de analisar?', (tokyo2020.event.unique()), key=2)
+with col4:
+    medley = pd.DataFrame(tokyo2020[(tokyo2020['heat']=='final') & (tokyo2020['event']== event_selected2)])
+    data = {
+        'Atleta': medley[('place')],
+        'split_50': medley['split_50'],
+        'split_100': medley['split_100'],
+        'split_150': medley['split_150'],
 
-tempoReacao = tokyo2020[(tokyo2020['event']== "women 50m freestyle") & (tokyo2020.heat.str.startswith('heat_'))]
-fig1 = px.scatter(tempoReacao, 
-                  x="reaction_time", 
-                  y="time", 
-                  trendline="ols", 
-                  title="Tempo de reação nas eliminatórias de 50m livre para mulheres")
-st.plotly_chart(fig1, use_container_width=True)
+        'split_200': medley['split_200'],
+        'split_250': medley['split_250'],
+        'split_300': medley['split_300'],
+        'split_350': medley['split_350'],
+        'split_400': medley['split_400'],
+    }
+    dfMedley2 = pd.DataFrame(data)
+    fig2 = px.parallel_coordinates(dfMedley2, 
+                                color="Atleta", 
+                                dimensions=dfMedley2.columns, 
+                                color_continuous_scale=px.colors.sequential.Turbo, 
+                                color_continuous_midpoint=4,
+                                title=f"Evento final: {event_selected}")
+    st.plotly_chart(fig2, use_container_width=True)
+
+st.subheader("Resultado final das medalhas conquistadas por país")
+col5, col6 = st.columns([1, 2.5])
+with col5:
+    final_df = tokyo2020[tokyo2020['heat'] == 'final']
+    first_place_df = final_df[final_df['place'] == 1]
+    second_place_df = final_df[final_df['place'] == 2]
+    third_place_df = final_df[final_df['place'] == 3]
+    teamGoldMedal = first_place_df.groupby('team').size().sort_values(ascending=False)
+    teamSilverMedal = second_place_df.groupby('team').size().sort_values(ascending=False)
+    teamBronzeMedal = third_place_df.groupby('team').size().sort_values(ascending=False)
+    teams_gold = np.array(first_place_df['team'].values)
+    teams_silver = np.array(second_place_df['team'].values)
+    teams_bronze = np.array(third_place_df['team'].values)
+    concat_teams = np.concatenate((teams_gold, teams_silver, teams_bronze))
+    unique_teams = np.unique(concat_teams)
+
+    medal_counts = pd.DataFrame({
+        'Time': unique_teams,
+        'Ouro': teamGoldMedal.reindex(unique_teams).values,
+        'Prata': teamSilverMedal.reindex(unique_teams).values,
+        'Bronze': teamBronzeMedal.reindex(unique_teams).values
+    })
+
+    show_gold = st.toggle("Mostrar Ouro", True)
+    show_silver = st.toggle("Mostrar Prata", True)
+    show_bronze = st.toggle("Mostrar Bronze", True)
 
 
-st.subheader("Finalistas Men 400m Individual Medley")
+    filtered_df = medal_counts[['Time']]
 
-medley = pd.DataFrame(tokyo2020[(tokyo2020['heat']=='final') & (tokyo2020['event']=='men 400m individual medley')])
-data = {
-    'Atleta': medley[('place')],
-    'split_50': medley['split_50'],
-    'split_100': medley['split_100'],
-    'split_150': medley['split_150'],
-    'split_200': medley['split_200'],
-    'split_250': medley['split_250'],
-    'split_300': medley['split_300'],
-    'split_350': medley['split_350'],
-    'split_400': medley['split_400'],
-}
-dfMedley2 = pd.DataFrame(data)
-fig2 = px.parallel_coordinates(dfMedley2, 
-                               color="Atleta", 
-                               dimensions=['Atleta','split_50', 'split_100', 'split_150', 'split_200', 'split_250', 'split_300', 'split_350', 'split_400'], 
-                               color_continuous_scale=px.colors.sequential.Turbo, 
-                               color_continuous_midpoint=4)
-st.plotly_chart(fig2, use_container_width=True)
+    if show_gold:
+        filtered_df['Ouro'] = medal_counts['Ouro']
+    if show_silver:
+        filtered_df['Prata'] = medal_counts['Prata']
+    if show_bronze:
+        filtered_df['Bronze'] = medal_counts['Bronze']
 
-st.subheader("Número e tipo de medalhas ganhadas e agrupadas por país")
+with col6:
+    df_melted_filtered = pd.melt(filtered_df, id_vars=['Time'], var_name='Medal', value_name='Count')
+    fig3 = px.bar(df_melted_filtered, x='Time', y='Count', color='Medal', color_discrete_sequence=['gold', 'silver', '#cd7f32'])
+    fig3.update_layout(
+        title='Total de Medalhas por País',
+        xaxis_title='País',
+        yaxis_title='Número de medalhas',
+        legend_title='Medal',
+        legend=dict(title='Medalha', orientation='h', y=1.1, x=0.5),
+        barmode='group',
+        showlegend=True
+    )
 
-final_df = tokyo2020[tokyo2020['heat'] == 'final']
-first_place_df = final_df[final_df['place'] == 1]
-second_place_df = final_df[final_df['place'] == 2]
-third_place_df = final_df[final_df['place'] == 3]
-teamGoldMedal = first_place_df.groupby('team').size().sort_values(ascending=False)
-teamSilverMedal = second_place_df.groupby('team').size().sort_values(ascending=False)
-teamBronzeMedal = third_place_df.groupby('team').size().sort_values(ascending=False)
-teams_gold = np.array(first_place_df['team'].values)
-teams_silver = np.array(second_place_df['team'].values)
-teams_bronze = np.array(third_place_df['team'].values)
-concat_teams = np.concatenate((teams_gold, teams_silver, teams_bronze))
-unique_teams = np.unique(concat_teams)
-
-medal_counts = pd.DataFrame({
-    'Team': unique_teams,
-    'Gold': teamGoldMedal.reindex(unique_teams).values,
-    'Silver': teamSilverMedal.reindex(unique_teams).values,
-    'Bronze': teamBronzeMedal.reindex(unique_teams).values
-})
-medal_counts['Total'] = medal_counts.sum(numeric_only= True, axis=1)
-medal_counts = medal_counts.sort_values(by=['Total','Gold','Silver','Bronze'], ascending=False)
-medal_counts.drop('Total', axis=1, inplace=True)
-df_melted = pd.melt(medal_counts, id_vars=['Team'], var_name='Medal', value_name='Count')
-fig3 = px.bar(df_melted, x='Team', y='Count', color='Medal', color_discrete_sequence=['gold','silver', '#cd7f32'])
-fig3.update_layout(
-    title='Total Medals by Country',
-    xaxis_title='Country',
-    yaxis_title='Number of Medals',
-    legend_title='Medal',
-    legend=dict(title='Medal', orientation='h', y=1.1, x=0.5),
-    barmode='group',
-    showlegend=True
-)
-st.plotly_chart(fig3, use_container_width=True)
+    # Exibindo o gráfico com o Streamlit
+    st.plotly_chart(fig3, use_container_width=True)
